a4447cfe0bb77a578cc0abe76dd5d70d2b530698
Chester Gould <chestergould@protonmail.com>
2023-12-21 19:11:46 -0800

    more ind nat things

--- examples/ind-nat.pp
+++ examples/ind-nat.pp
@@ -23,6 +23,28 @@
 
 (def [two : Nat] (s (s z)))
 
+(def
+  [rec-Nat :
+    (Π [l : ((Level 0) : (Type 1 lzero))]
+      (Π [A : (Type 0 l)]
+        (Π A
+          (-> (-> Nat (-> A A))
+            (-> Nat A)))))]
+  (λ l (λ A (λ z-case (λ s-case (λ n
+    (ind-W l n (λ _ A)
+      (λ tag (λ d (λ ih
+        ((ind-Bool l tag (λ t (-> (-> (Nat-Arity t) Nat) (-> (-> (Nat-Arity t) A) A)))
+           (λ d (λ ih (s-case (d ()) (ih ()))))
+           (λ d (λ ih z-case)))
+         d ih)))))))))))
+
+(def [+ : (-> Nat (-> Nat Nat))]
+  (λ a (λ b
+    (rec-Nat (lzero : ((Level 0) : (Type 1 lzero))) Nat
+      b
+      (λ _ (λ a+b (s a+b)))
+      a))))
+
 #;
 (def
   [ind-Nat :
@@ -34,24 +56,11 @@
   (λ l (λ m (λ z-case (λ s-case (λ n
     (ind-W l n m
       (λ tag (λ d (λ ih
-        ((ind-Bool l tag (λ t (Π [eq : (= (Bool : (Type 0 lzero)) t tag)] (m (w t d))))
-          (λ eq
-            (s-case
-              (d (ind-= (lzero : ((Level 0) : (Type 1 lzero))) tag eq (λ tag (λ _ (Nat-Arity tag))) ()))
-              (ih (ind-= (lzero : ((Level 0) : (Type 1 lzero))) tag eq (λ tag (λ _ (Nat-Arity tag))) ()))))
-          (λ eq z-case)) Refl)))))))))))
-
-(def
-  [ind-Nat :
-    (Π [l : ((Level 0) : (Type 1 lzero))]
-      (Π [m : (-> Nat (Type 0 l))]
-        (Π [z-case : (m z)]
-          (Π [s-case : (Π [n : Nat] (-> (m n) (m (s n))))]
-            (Π [n : Nat] (m n))))))]
-  (λ l (λ m (λ z-case (λ s-case (λ n
-    (ind-W l n m
-      (λ tag (λ d (λ ih
-        ((ind-Bool l tag (λ t (Π [d : (-> (Nat-Arity t) Nat)] (Π [ih : (-> [i : (Nat-Arity t)] (m (d i)))] (m (w t d)))))
+        ((ind-Bool l tag
+          (λ t
+            (Π [d : (-> (Nat-Arity t) Nat)]
+              (Π [ih : (-> [i : (Nat-Arity t)] (m (d i)))]
+                (m (w t d)))))
           (λ d (λ ih
             (s-case
               (d ())
